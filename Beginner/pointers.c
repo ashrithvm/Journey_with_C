f8c95f73095170f4fdcb0dde207e82d0e215d47d
ashrith <ashrith.mudundi@gmail.com>
2024-04-03 11:46:06 -0400

    testing basic arrays and their theory

--- Beginner/pointers.c
+++ Beginner/pointers.c
@@ -3,11 +3,14 @@
 #include <stdio.h>
 
 void pointerTest();
+void arrayTest();
 
 int main(){
-    pointerTest();
+    //pointerTest();
+    arrayTest();
 }
 
+/// @brief Testing how basic pointers work
 void pointerTest(){
     int n; //declare variable 'n' as an integer
     int* ptr; //declare that item at <ptr> address is an integer
@@ -19,4 +22,12 @@ void pointerTest(){
     *ptr=99;//change value of item at <ptr> address
 
     printf("var: %d, ptr: %d\n", n, *ptr);
+}
+
+/// @brief Testing how a basic array declaration and indexing work
+void arrayTest(){
+    float arr[5]={1.1,2.2,3.3,4.4,5.5};
+    float* ptr = &arr[0];
+    printf("%f\n",*ptr);
+    printf("%f\n",*(ptr+1));
 }
